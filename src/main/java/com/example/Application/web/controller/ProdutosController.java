b29959807f3e8841871cf1717f3be70ea84ba63f
joao guilherme <joao@joao>
2018-05-30 20:14:36 -0300

    nao consigo fazer o botao de cadastrar

--- src/main/java/com/example/Application/web/controller/ProdutosController.java
+++ src/main/java/com/example/Application/web/controller/ProdutosController.java
@@ -1,9 +1,17 @@
 package com.example.Application.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mballem.curso.boot.domain.Produto;
 import com.mballem.curso.boot.service.ProdutoService;
 
 @Controller
@@ -12,31 +20,52 @@ public class ProdutosController {
 	
 	private ProdutoService service;
 	
-	@GetMapping("/alface")
-	public String exibeAlface() {
-      return "/produtos/alface";
-        
-	}
-	
-	
-	
-	
-	@GetMapping("/alho")
-	public String exibeAlho() {
-		return "/produtos/alho";
-	}
-	
-	@GetMapping("/cenoura")
-	public String exibeCenoura() {
-		return "/produtos/cenoura";
-	}
-	
 	@GetMapping("/repolho")
 	public String exibeRepolho() {
-		return "/produtos/repolho";
+		return "produtos/repolho";
+	}
+	
+	@GetMapping("/cadastrar")
+	 public String cadastrar(Produto produto) {
+		 return "produtos/cadastro";
+	 }
+ 
+	@GetMapping("/listar")
+	public String listar(ModelMap model) {
+		model.addAttribute("produto",service.buscarTodos());
+		return "produtos/listar";
+	}
+	
+	
+	@PostMapping("/salvar")
+	public String salvar(@Valid Produto produto ,BindingResult result , RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return"produtos/cadastro";
+		}
+		
+		service.salvar(produto);
+		attr.addFlashAttribute("sucesso", "produto registrado com sucesso");
+		
+		
+		return "redirect:/produtos/cadastro";
 	}
 	
 	
 	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id,ModelMap model) {
+		
+		service.excluir(id);
+        model.addAttribute("sucesso", "departamento excluido com sucesso");	
+	
+	
+    return listar(model);
+}
 
+	
+	
+	
+	
+	
 }
